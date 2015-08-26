2961fdb1ce5f9213e9afa5296fae0f4dae836982
anthonymiyoro <amiyoro@gmail.com>
2015-08-26 10:34:52 +0300

    Fixed creation error

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,15 +4,15 @@ class JobsController < ApplicationController
   end
 
   def new
-      @job = Job.new(params.require(:job).permit(:title, :company, :url))
+      @job = Job.new
   end
 
     def create
-        @job = Job.new
+        @job = Job.new(params.require(:job).permit(:title, :company, :url))
         if @job.save
             redirect_to root_path
         else
             render "new"
     end
-end
+  end
 end
